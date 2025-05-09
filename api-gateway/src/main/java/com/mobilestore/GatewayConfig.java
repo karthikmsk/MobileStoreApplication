ee22a7d2125be740a5951f05aeb8f6aff231a0c9
Karthik M S <karthik.ms@sonata-software.com>
2025-05-09 11:59:26 +0530

    Added API Gateway service for routing and load balancing

---
+++ api-gateway/src/main/java/com/mobilestore/GatewayConfig.java
@@ -0,0 +1,19 @@
+package com.mobilestore;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+//     @Bean
+//     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+//         return builder.routes()
+//             // Route configuration for product-service
+//             .route("product-service", r -> r.path("/product-service/**")
+//                 .filters(f -> f.stripPrefix(1))
+//                 .uri("lb://PRODUCT-SERVICE"))
+//             // Route configuration for order-service
+       
+//             .build();
+//     }
+ }
