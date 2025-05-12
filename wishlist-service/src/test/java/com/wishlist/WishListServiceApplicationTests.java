85aea0c5f05aabc5770f45fd697806d98f4b16bf
KARTHIK M S <karthikms207@gmail.com>
2025-05-12 05:41:10 +0000

    Updated all Dockerfiles in each services

--- wishlist-service/src/test/java/com/wishlist/WishListServiceApplicationTests.java
+++ wishlist-service/src/test/java/com/wishlist/WishListServiceApplicationTests.java
@@ -1,10 +1,23 @@
 package com.wishlist;
 
+import com.wishlist.Client.ProductClient;
+import com.wishlist.Client.UserClient;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WishListServiceApplicationTests {
+
+	@MockBean
+    private ProductClient productClient;
+
+    @MockBean
+    private UserClient userClient;
+
+    @Autowired
+    private WishListService wishListService;
 
 	@Test
 	void contextLoads() {
